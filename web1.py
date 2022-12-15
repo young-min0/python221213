7b1db434d3dea4c3a200e24a8f671ff820fb8c23
young-min0 <niceguuy@naver.com>
2022-12-15 14:50:57 +0900

    웹 크롤링 연습2

--- web1.py
+++ web1.py
@@ -1,9 +1,19 @@
 # web1.py
 from bs4 import BeautifulSoup
 
-# 페이지 로딩
-page = open(r"c:\work\test01.html", "rt", encoding="utf-8").open()
+# 페이지 로딩: 메서드 체인 (연속으로 함수, 메서드 호출)
+page = open(r"c:\work\test01.html", "rt", encoding="utf-8").read()
 # 검색이 용이한 객체
 soup = BeautifulSoup(page, "html.parser")
 # 전체 페이지 보기
-print(soup.prettify())
+# print(soup.prettify())
+
+# <p> 태그 전부 검색
+# print(soup.find_all("p"))
+
+# 첫 번째 <p> 태그
+# print(soup.find("p"))
+
+# 검색의 조건: <p class='outer-text'>"
+# 파이썬의 키워드 class 제공(이름 충돌): class_ 
+print(soup.find_all("p", class_='outer-text'))
